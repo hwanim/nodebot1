bc84677e6fb5351a60f42f683d0987f7b5bd82ca
임환 <blank_huan@imhwan-ui-iMac.local>
2018-07-12 18:16:43 +0900

    bot function add

--- app.js
+++ app.js
@@ -7,4 +7,34 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'jade')
   .get('/', (req, res) => res.send('hi!'))
+  .get('/keyboard', (req, res) => {
+  const menu = {
+      type: 'buttons',
+      buttons: ["메뉴1", "메뉴2", "메뉴3"]
+  };
+  res.set({
+      'content-type': 'application/json'
+  }).send(JSON.stringify(menu));})
+  .post('/message', (req, res) => {
+    const _obj = {
+        user_key: req.body.user_key,
+        type: req.body.type,
+        content: req.body.content
+    };
+    let massage = {
+        "message": {
+            "text": '응답 메세지...'
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": [
+                "메뉴1",
+                "메뉴2",
+                "메뉴3"
+            ]
+        }
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(massage));})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
